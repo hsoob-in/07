404d99b8197c974d8a3619577bfe6f2dd96b9795
hsoob-in <tnqls2361@sookmyung.ac.kr>
2025-10-16 16:04:05 +0900

    실습06-1

--- main.c
+++ main.c
@@ -1,24 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-void sub(void)
-{        
-  int auto_count = 0; //초기화됨(자동할당) 
-  static int static_count = 0; //static 변수는 값을 계속 저장함(정적할당) 
-  auto_count++;
-  static_count++;
-  
-  printf("auto_count = %d\n", auto_count);
-  printf("static_count = %d\n", static_count);
-
-}
+int all_files;
+extern void sub();
 
 int main(void)
-{
-  int i;
-  for(i=0; i<3; i++)
+{        
   sub();
+  printf("%d\n", all_files);
   
   system("PAUSE");
   return 0;
